3858d2f330e89ddcf318cd36da79870926751309
raman <kukreti.rs@gmail.com>
2020-07-11 17:10:45 +0530

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 app.set('view-engine', 'ejs');
+app.set("port", process.env.PORT || 5000);
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render('home_page.ejs');
 });
-app.listen('8000', (error) => {
+app.listen(app.get("port"), (error) => {
   if (error) {
     throw error;
   } else {
