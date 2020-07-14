139c26fb46f88b06cf2ad1857f94c2d1ff4304d2
raman <kukreti.rs@gmail.com>
2020-07-14 06:22:44 +0530

    refactor code

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const morgan = require('morgan');
-const istSemester = require('./routes/istsem');
-const iindSemester = require('./routes/iindsem');
-const iiirdSemester = require('./routes/iiirdsem');
-const ivthSemester = require('./routes/ivthsem');
-const home = require('./routes/home');
+const ejs = require("ejs");
+const morgan = require("morgan");
+const istSemester = require("./routes/istsem");
+const iindSemester = require("./routes/iindsem");
+const iiirdSemester = require("./routes/iiirdsem");
+const ivthSemester = require("./routes/ivthsem");
+const home = require("./routes/home");
 
-app.set('view-engine', 'ejs');
+app.set("view-engine", "ejs");
 app.set("port", process.env.PORT || 5000);
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app
-  .get(['/', '/home'], home)
-  .get('/firstsemester', istSemester)
-  .get('/secondsemester', iindSemester)
-  .get('/thirdsemester', iiirdSemester)
-  .get('/fourthsemester', ivthSemester)
-
+  .get(["/", "/home"], home)
+  .get("/firstsemester", istSemester)
+  .get("/secondsemester", iindSemester)
+  .get("/thirdsemester", iiirdSemester)
+  .get("/fourthsemester", ivthSemester);
 
 app.listen(app.get("port"), (error) => {
   if (error) {
     throw error;
   } else {
-    console.log("server is running at 8000");
+    console.log(`at 5000`);
   }
 });
